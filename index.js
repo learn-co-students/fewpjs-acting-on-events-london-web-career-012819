c0837ad85bbf05db74d4b3df4872d60ce7a1e799
stubrew24 <sewelly@gmail.com>
2019-03-11 15:03:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+document.addEventListener('keydown', function(e) {
+    dodger = document.getElementById('dodger')
+
+    if(e.which === 37){
+        moveDodgerLeft()
+    }
+
+    if(e.which === 39){
+        moveDodgerRight()
+    }
+
+})
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    if(left > 0){
+        dodger.style.left = `${left -1 }px`
+    }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    if(left > 0){
+        dodger.style.left = `${left + 1 }px`
+    }
+}
